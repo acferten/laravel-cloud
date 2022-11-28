0992c1df1da9baf91fec27f7a003ac6f924b4fe9
acferten <gowiththeirname@gmail.com>
2022-11-28 12:56:30 +0700

    создание папок

--- www/routes/api.php
+++ www/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FolderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\AuthController;
@@ -7,7 +8,7 @@ use \App\Http\Controllers\AuthController;
 
 Route::post('/registration', [AuthController::class, 'registration']);
 Route::post('/authorization', [AuthController::class, 'login']);
-
+Route::post('/folders', [FolderController::class, 'create']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
